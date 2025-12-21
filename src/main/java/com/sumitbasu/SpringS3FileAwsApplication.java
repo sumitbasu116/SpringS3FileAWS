bb96e4e036539f2a9fa01fba8d75d81a4041d5f7
Sumit <sumitbasu116@gmail.com>
2025-12-21 19:28:57 +0530

    code first commit

---
+++ src/main/java/com/sumitbasu/SpringS3FileAwsApplication.java
@@ -0,0 +1,13 @@
+package com.sumitbasu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringS3FileAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringS3FileAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumitbasu/SpringS3FileAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumitbasu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringS3FileAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
